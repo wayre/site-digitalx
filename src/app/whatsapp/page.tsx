cb4ceae99344b4eecd0bed964e5b6545e0374e2b
Wayre Avelar <wayre1@gmail.com>
2025-07-31 11:37:59 -0300

    Fix: Ajuste mensagem whatsapp

--- src/app/whatsapp/page.tsx
+++ src/app/whatsapp/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function WhatsappRedirectPage() {
   useEffect(() => {
     window.location.href =
-      "https://api.whatsapp.com/send?phone=5567992767508&text=Ol%C3%A1%2C%20vim%20do%20site%20da%20DigitalX%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es.";
+      "https://api.whatsapp.com/send?phone=5567992767508&text=Olá,%20vim%20do%20site%20da%20DigitalX%20e%20gostaria%20de%20fazer%20alguns%20exames%20solicitados%20pelo%20meu%20dentista.";
   }, []);
 
   // Como o redirect() interrompe a renderização,
