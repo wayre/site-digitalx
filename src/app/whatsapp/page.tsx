afd1c49063d9d56782cac2187915a34250207c9d
Wayre Avelar <wayre1@gmail.com>
2025-08-01 08:20:39 -0300

    add: New Updates List

--- src/app/whatsapp/page.tsx
+++ src/app/whatsapp/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function WhatsappRedirectPage() {
   useEffect(() => {
     window.location.href =
-      "https://api.whatsapp.com/send?phone=5567992767508&text=Olá,%20vim%20do%20site%20da%20DigitalX%20e%20gostaria%20de%20fazer%20alguns%20exames%20solicitados%20pelo%20meu%20dentista.";
+      "https://api.whatsapp.com/send?phone=5567992767508&text=Olá,%20gostaria%20de%20mais%20informações.";
   }, []);
 
   // Como o redirect() interrompe a renderização,
